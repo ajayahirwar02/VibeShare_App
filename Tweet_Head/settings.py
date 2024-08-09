88fce43418a8a59bcac5d172e82fecd9aec0d92e
Ajay Ahirwar <120614440+ajayahirwar02@users.noreply.github.com>
2024-08-10 01:23:02 +0530

    Update settings.py

--- Tweet_Head/settings.py
+++ Tweet_Head/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS=[
 ]
 
 import os
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT= BASE_DIR/"media"
 MEDIA_URL="/media/"
